0382517fb99d73dd7748bd10877b54da649e135f
Sofa <sofviic1993@gmail.com>
2021-05-20 16:03:08 +0300

    Implemented Conway's Game Of Life.

--- project/Program.cs
+++ project/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Linq;
 
 namespace cstest {
 	class MainClass {
@@ -36,7 +37,6 @@ namespace cstest {
 
 		private void Draw(object sender, PaintEventArgs e) {
 			Pen pen = new Pen(Color.LemonChiffon, 1f);
-
 			for(int j = 0; j < gridSize.Height; ++j)
 				for(int i = 0; i < gridSize.Width; ++i)
 					if(ca[i, j] == 1)
@@ -46,6 +46,8 @@ namespace cstest {
 		Size cellSize;
 		Size gridSize;
 		int[,] ca;
+
+		Func<int[,], bool> conway;
 
 		private void Init() {
 			cellSize = new Size(10, 10);
@@ -60,10 +62,18 @@ namespace cstest {
 			};
 
 			pictureBox.Paint += Draw;
+			pictureBox.Click += Tick;
 			Controls.Add(pictureBox);
 
 			ca = new int[gridSize.Width, gridSize.Height];
-			ca = FillCA(ca, (i, j) => (j + i) % 2 == 0);
+			ca = FillCA(ca, (i, j) => j == 3 && i > 0 && i < 4 || j == 2 && i == 3 || j == 1 && i == 2);
+
+			conway = g => Flatten(g).Sum() == 3 || (Flatten(g).Sum() == 4 && g[1, 1] == 1);
+		}
+
+		private void Tick(object sender, EventArgs e) {
+			ca = UpdateCA(ca, conway);
+			Refresh();
 		}
 
 		int[,] FillCA(int[,] grid, Func<int,int,bool> f){
@@ -76,6 +86,81 @@ namespace cstest {
 			return grid;
 		}
 
+		int[,] UpdateCA(int[,] grid, Func<int[,], bool> f) {
+			int x = grid.GetLength(0);
+			int y = grid.GetLength(1);
+			int[,] res = (int[,])grid.Clone();
+			int[,][,] convGrid = ConvoluteGrid(grid);
+			for(int j = 0; j < y; ++j)
+				for(int i = 0; i < x; ++i)
+					res[i, j] = f(convGrid[i, j]) ? 1 : 0;
+			return res;
+		}
+
+		T[,][,] ConvoluteGrid<T>(T[,] grid) {
+			int x = grid.GetLength(0);
+			int y = grid.GetLength(1);
+			// ie, T[,][,] res = new T[x, y][3, 3];
+			T[,][,] res = new T[x, y][,];
+			for(int j = 0; j < y; ++j)
+				for(int i = 0; i < x; ++i)
+					res[i,j] = new T[3, 3];
+			
+			for(int j = 0; j < y; ++j)
+				for(int i = 0; i < x; ++i){
+					res[i, j] = new T[,]{
+						{ grid[Mod(i - 1, x), Mod(j - 1, y)], grid[Mod(i - 0, x), Mod(j - 1, y)], grid[Mod(i + 1, x), Mod(j - 1, y)] },
+						{ grid[Mod(i - 1, x), Mod(j - 0, y)], grid[Mod(i - 0, x), Mod(j - 0, y)], grid[Mod(i + 1, x), Mod(j - 0, y)] },
+						{ grid[Mod(i - 1, x), Mod(j + 1, y)], grid[Mod(i - 0, x), Mod(j + 1, y)], grid[Mod(i + 1, x), Mod(j + 1, y)] }
+					};
+				}
+			return res;
+		}
+
 		Size Mul(Size a, Size b) => new Size(a.Width * b.Width, a.Height * b.Height);
+		int Mod(int a, int b) => ((a % b) + b) % b;
+
+		// Pretty(kinda) Print
+		string Print<T>(T[] arr) {
+			int x = arr.Length;
+			string res = "";
+			for(int i = 0; i < x; ++i)
+				res += arr[i] + " ";
+			return res;
+		}
+
+		string Print<T>(T[,] grid) {
+			int x = grid.GetLength(0);
+			int y = grid.GetLength(1);
+			string res = "";
+			for(int j = 0; j < y; ++j) {
+				for(int i = 0; i < x; ++i)
+					res += grid[i, j] + " ";
+				res += "\n";
+			}
+			return res;
+		}
+
+		string Print2<T>(T[,][,] grid) {
+			int x = grid.GetLength(0);
+			int y = grid.GetLength(1);
+			string res = "";
+			for(int j = 0; j < y; ++j)
+				for(int i = 0; i < x; ++i)
+					res += Print(grid[i, j]) + "\n\n\n";
+			return res;
+		}
+
+		// Array Manipulation
+		T[] Flatten<T>(T[,] arr) {
+			int x = arr.GetLength(0);
+			int y = arr.GetLength(1);
+			T[] res = new T[x * y];
+			for(int i = 0; i < x; ++i)
+				for(int j = 0; j < y; ++j)
+					res[i + j * x] = arr[i, j];
+			return res;
+		}
+		T[] Drop<T>(T[] arr, int i) => arr.Take(i).Concat(arr.Skip(i + 1)).ToArray();
 	}
 }
